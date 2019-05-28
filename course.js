cdf0184e8167397d8c36c7e9d8d8a74f6ca0fb33
jose julio <julio619alves@gmail.com>
2019-05-28 09:28:17 -0300

    crud course completed

--- course.js
+++ course.js
@@ -7,7 +7,6 @@ var id = 0;
 var courses = [];
 
 router.post('/', function (req, res) { //CREATE
-    
     var course = req.body;
     course['id']=id++;
     for(var i = 0; i<course.teacher.length; i++){
@@ -17,9 +16,7 @@ router.post('/', function (req, res) { //CREATE
         }else{
             course.teacher[i] = '';
         }
-        
     }
-
     courses.push(course);
     res.send('Curso cadastrado com sucesso!');
   })
@@ -38,6 +35,36 @@ router.post('/', function (req, res) { //CREATE
     }
   })
   
+  router.put('/:id', function (req, res) { //CREATE
+    var id = req.params.id;
+    for (var i=0; i<courses.length; i++){
+      if (id == courses[i]['id']){
+        var course = req.body;
+        courses[i]['name']= course.name || courses[i]['name'];
+        courses[i]['period']= course.period || courses[i]['period'];
+        courses[i]['city']= course.city || courses[i]['city'];
+        courses[i]['teacher']= course.teacher || courses[i]['teacher'];
+        if (course.teacher){
+          for(var j = 0; j<courses[i].teacher.length; j++){
+            var idTeacher = courses[i].teacher[j];
+            if(arqTeacher.findbyId(idTeacher)){
+                courses[i].teacher[j] = arqTeacher.findbyId(idTeacher);
+            }else{
+                courses[i].teacher[j] = '';
+            }
+        }
+        }
+        
+      res.send('Curso editado com sucesso!');
+      }else if(i == courses.length-1){
+        res.status(404).send('Curso não encontrado');
+      }
+    }
+    if (courses.length == 0){
+      res.status(404).send('Nenhum curso cadastrado')      
+    }
+  })
+
   router.delete('/', function (req, res) { //DELETE ALL
     courses = [];
     res.send('Todos os Cursos foram removidos com sucesso!');
@@ -60,7 +87,6 @@ router.post('/', function (req, res) { //CREATE
 
     function findbyId(idCourse){
         idCourse = parseInt(idCourse);
-        console.log(idCourse);
         for (var i = 0; i<courses.length; i++){
           if (idCourse == courses[i]['id']){
              return courses[i];
