0947ae98edff1c5ccab2f75752e4680cd930d166
jknvlvxs <julio619alves@gmail.com>
2019-06-06 17:58:33 -0300

    fixing mvc, moving business rules to model

--- controller/user.js
+++ controller/user.js
@@ -6,18 +6,7 @@ exports.getAllUsers = (req, res) => {
     let projection = {projection: {_id:0, id: 1, name: 1, lastName: 1, profile:1}};
 
     // send to model
-    userModel.getAll(query, projection)
-    .then(users => {
-        if(users.length > 0){ 
-            res.status(200).send(users);        
-        }else{
-            res.status(404).send('Nenhum usuário cadastrado');
-        }
-    })
-    .catch(err => {
-        console.error("Erro ao conectar a collection user: ", err);
-        res.status(500);
-    });
+    return userModel.getAll(res, query, projection);
 };
 
 exports.getFilteredUser = (req,res) => {
@@ -26,81 +15,31 @@ exports.getFilteredUser = (req,res) => {
     let projection = {projection: {_id:0, id: 1, name: 1, lastName: 1, profile:1}};
 
     // send to model
-    userModel.getFiltered(query, projection)
-    .then(user => {
-        if(user.length > 0){
-            res.status(200).send(user);        
-        }else{
-            res.status(404).send('O usuário não foi encontrado');
-        }
-    })
-    .catch(err => {
-        console.error("Erro ao conectar a collection user: ", err);
-        res.status(500);
-    });
+    return userModel.getFiltered(res, query, projection)
 };
 
 exports.postUser = (req, res) => {
-    // check required attributes
-    if(req.body.name && req.body.lastName && req.body.profile){
-        
-        // creates user array to be inserted
-        let user = {
-            id:0,
-            name:req.body.name,
-            lastName:req.body.lastName,
-            profile:req.body.profile,
-            status:1
-        };
+    // creates user array to be inserted
+    let user = {
+        id:0,
+        name:req.body.name,
+        lastName:req.body.lastName,
+        profile:req.body.profile,
+        status:1
+    };
 
-            // send to model
-            userModel.post(user)
-            .then(result => {
-                if(result != false){
-                    res.status(201).send('Usuário cadastrado com sucesso!');
-                }else{
-                    res.status(401).send('Não foi possível cadastrar o usuário (profile inválido)');
-                }
-            })
-            .catch(err => {
-                console.error("Erro ao conectar a collection user: ", err);
-                res.status(500);
-            });
-        }else{
-            res.status(401).send('Não foi possível cadastrar o usuário (profile inválido)');
-        }
+    // send to model
+    userModel.post(res, user)
 };
 
 exports.putUser = (req, res) => {
-    // check required attributes
-    if(req.body.name && req.body.lastName && req.body.profile){
 
-        //  define query and set for search and update    
-        let query = {'id': parseInt(req.params.id), 'status': 1};
-        let set = {name: req.body.name, lastName: req.body.lastName, profile: req.body.profile};
+    //  define query and set for search and update    
+    let query = {'id': parseInt(req.params.id), 'status': 1};
+    let set = {name: req.body.name, lastName: req.body.lastName, profile: req.body.profile};
         
-            // send to model
-            userModel.put(query, set)
-            .then(result => {
-                if(result != false){
-                    if(result.value){ // if user exists
-                        res.status(200).send('Usuário editado com sucesso!');
-                    }else{
-                        res.status(401).send('Não é possível editar usuário inexistente');
-                    }
-                }else{
-                    res.status(401).send('Não é possível editar usuário (profile inválido)');                    
-                }
-            })
-            
-            .catch(err => {
-                console.error("Erro ao conectar a collection user: ", err);
-                res.status(500);
-            });
-        
-    }else{
-        res.status(401).send('Não foi possível editar o usuário');
-    }
+    // send to model
+    userModel.put(res, query, set)
 };
 
 exports.deleteUser = (req, res) => {
@@ -108,18 +47,5 @@ exports.deleteUser = (req, res) => {
     let query = {'id': parseInt(req.params.id), 'status':1};
 
     // send to model
-    userModel.delete(query)
-    .then(result => {
-        if(result.value){ // if user exists
-            console.log('O usuário foi removido');
-            res.status(200).send('O usuário foi removido com sucesso');
-          }else{
-            console.log('Nenhum usuário foi removido');
-            res.status(204).send();
-          }
-    })
-    .catch(err => {
-        console.error("Erro ao conectar a collection user: ", err);
-        res.status(500);
-    });
+    userModel.delete(res, query)
 };
--- model/user.js
+++ model/user.js
@@ -7,35 +7,95 @@ var id;
      id = await collection.countDocuments({});
 })();
 
-exports.getAll = (query, projection) => {
-    return collection.find(query, projection).toArray();
+exports.getAll = (res, query, projection) => {
+    return collection.find(query, projection).toArray()
+        .then(users => {
+        if(users.length > 0){ 
+            res.status(200).send(users);        
+        }else{
+            res.status(404).send('Nenhum usuário cadastrado');
+        }
+    })
+    .catch(err => {
+        console.error("Erro ao conectar a collection user: ", err);
+        res.status(500);
+    });
 };
 
-exports.getFiltered = (query, projection) => {
-    return collection.find(query, projection).toArray();
+exports.getFiltered = (res, query, projection) => {
+    return collection.find(query, projection).toArray()
+    .then(user => {
+        if(user.length > 0){
+            res.status(200).send(user);        
+        }else{
+            res.status(404).send('O usuário não foi encontrado');
+        }
+    })
+    .catch(err => {
+        console.error("Erro ao conectar a collection user: ", err);
+        res.status(500);
+    });
 };
 
-exports.post = (user) => {
-    if(user.profile == 'guess' || user.profile == 'admin'){
+exports.post = (res, user) => {
+    // check required attributes
+    if(user.name && user.lastName && user.profile && user.profile == 'guess' || user.profile == 'admin'){
         user.id = ++id;
-        return collection.insertOne(user);
-    }else{
-        return new Promise((resolve, reject) => {
-            resolve(false);
+        return collection.insertOne(user)
+        .then(result => {
+            if(result != false){
+                res.status(201).send('Usuário cadastrado com sucesso!');
+            }else{
+                res.status(401).send('Não foi possível cadastrar o usuário (profile inválido)');
+            }
+        })
+        .catch(err => {
+            console.error("Erro ao conectar a collection user: ", err);
+            res.status(500);
         });
+    }else{
+        res.status(401).send('Não foi possível cadastrar o usuário (profile inválido)');
     }
 };
 
-exports.put = (query, set) => {
-    if(set.profile == 'guess' || set.profile == 'admin'){
-        return collection.findOneAndUpdate(query, {$set: set});
-    }else{
-        return new Promise((resolve, reject) => {
-            resolve(false);
+exports.put = (res, query, user) => {
+    // check required attributes
+    if(user.name && user.lastName && user.profile && user.profile == 'guess' || user.profile == 'admin'){
+        return collection.findOneAndUpdate(query, {$set: user})
+        .then(result => {
+            if(result != false){
+                if(result.value){ // if user exists
+                    res.status(200).send('Usuário editado com sucesso!');
+                }else{
+                    res.status(401).send('Não é possível editar usuário inexistente');
+                }
+            }else{
+                res.status(401).send('Não é possível editar usuário (profile inválido)');                    
+            }
+        })
+        
+        .catch(err => {
+            console.error("Erro ao conectar a collection user: ", err);
+            res.status(500);
         });
+    }else{
+        
     }
 };
 
-exports.delete = (query) => {
-    return collection.findOneAndUpdate(query, {$set: {status:0}});
+exports.delete = (res, query) => {
+    return collection.findOneAndUpdate(query, {$set: {status:0}})
+    .then(result => {
+        if(result.value){ // if user exists
+            console.log('O usuário foi removido');
+            res.status(200).send('O usuário foi removido com sucesso');
+          }else{
+            console.log('Nenhum usuário foi removido');
+            res.status(204).send();
+          }
+    })
+    .catch(err => {
+        console.error("Erro ao conectar a collection user: ", err);
+        res.status(500);
+    });
 };
