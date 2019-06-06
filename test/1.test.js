64d65b5f6f6aa9510c319805ccf011d170c3ce48
jknvlvxs <julio619alves@gmail.com>
2019-06-06 09:57:07 -0300

    removes delete from test

--- test/1.test.js
+++ test/1.test.js
@@ -5,15 +5,11 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Aguardando banco...', function() {
-
   this.timeout(12000);
-
   before(done => {
-
     setTimeout(() => {
       done();
     }, 10000);
-
   });
 
   it('Testing Connection...', function() {
@@ -23,5 +19,60 @@ describe('Aguardando banco...', function() {
           assert.equal(res.status, 200)
       })
   });
-  
 });
+
+// describe('DELETE /api/v1/user', function() {
+//   it('it should DELETE user 1', () => {
+//     return request(app)
+//     .delete('/api/v1/user/1')
+//     .then(function(res) { 
+//       assert.equal(res.status, 200);
+//     });
+//   });
+
+//   it('it should DELETE user 2', () => {
+//     return request(app)
+//     .delete('/api/v1/user/2')
+//     .then(function(res) { 
+//       assert.equal(res.status, 200);
+//     });
+//   });
+// });
+
+// describe('DELETE /api/v1/teacher', function() {
+//   it('it should DELETE teacher 1"', () => {
+//     return request(app)
+//     .delete('/api/v1/teacher/1')
+//     .then(function(res) { 
+//       assert.equal(res.status, 200);
+//     });
+//   });
+
+//   it('it should DELETE teacher 2', () => {
+//     return request(app)
+//     .delete('/api/v1/teacher/2')
+//     .then(function(res) { 
+//       assert.equal(res.status, 200);
+//     });
+//   });
+// });
+
+// describe('DELETE /api/v1/course', function() {
+//   it('it should DELETE course 1', () => {
+//     return request(app)
+//     .delete('/api/v1/course/1')
+//     .then(function(res) { 
+//       assert.equal(res.status, 200);
+//     });
+//   });
+// });
+
+// describe('DELETE /api/v1/student', function() {
+//   it('it should DELETE student 1', () => {
+//     return request(app)
+//     .delete('/api/v1/student/1')
+//     .then(function(res) { 
+//       assert.equal(res.status, 200);
+//     });
+//   });
+// });
