0017d72cbdc5942dbf82e8a47b7edb704bd7e447
jknvlvxs <julio619alves@gmail.com>
2019-06-07 12:25:49 -0300

    ident app and database

--- app.js
+++ app.js
@@ -8,28 +8,26 @@ app.use(bodyParser.json());
 const database = require('./database');
 
 database
-  .connect()
-  .then(() => {
-
+.connect()
+.then(() => {
     app.get(baseAPI, function (req, res) {
-      res.status(200).send('Hello World!');
-    });
-  
-app.use(baseAPI+'/student', require('./routes/student'));
-app.use(baseAPI+'/user', require('./routes/user'));
-app.use(baseAPI+'/course', require('./routes/course'));
-app.use(baseAPI+'/teacher', require('./routes/teacher'));
+      	res.status(200).send('Hello World!');
+	});
+	
+	app.use(baseAPI+'/student', require('./routes/student'));
+	app.use(baseAPI+'/user', require('./routes/user'));
+	app.use(baseAPI+'/course', require('./routes/course'));
+	app.use(baseAPI+'/teacher', require('./routes/teacher'));
 
-app.get(baseAPI+'/', function (req, res){
-  res.send('Endpoints: \n '+baseAPI+'/user \n '+baseAPI+'/student \n '+baseAPI+'/course \n '+baseAPI+'/teacher');
-});
+	app.get(baseAPI+'/', function (req, res){
+	  	res.send('Endpoints: \n '+baseAPI+'/user \n '+baseAPI+'/student \n '+baseAPI+'/course \n '+baseAPI+'/teacher');
+	});
 
-app.get('/'+baseAPI, function (req, res){
-  res.send('Endpoints: \n /user \n /student \n /course \n /teacher');
-});
+	app.get('/'+baseAPI, function (req, res){
+	  	res.send('Endpoints: \n /user \n /student \n /course \n /teacher');
+	});
 
-app.listen(process.env.PORT || 3000);
-
+	app.listen(process.env.PORT || 3000);
 });
 
 module.exports = app;
