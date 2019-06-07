f8815b336b584f7f33577cdd0cd61cd7c137527d
Jose <julio619alves@gmail.com>
2019-06-06 23:00:54 -0300

    moving teacher business rules from controller to model

--- controller/teacher.js
+++ controller/teacher.js
@@ -1,6 +1,4 @@
 const teacherModel = require('../model/teacher');
-const courseModel = require('../model/course');
-const studentModel = require('../model/student');
 
 exports.getAllTeachers = (req, res) => {
     //  define query and projection for search
@@ -8,18 +6,7 @@ exports.getAllTeachers = (req, res) => {
     let projection = {projection: {_id:0, id: 1, name: 1, lastName: 1, phd:1}};
 
     // send to model
-    teacherModel.getAll(query, projection)
-    .then(teachers => {
-        if(teachers.length > 0){
-            res.status(200).send(teachers);        
-        }else{
-            res.status(404).send('Nenhum professor cadastrado');
-        }
-    })
-    .catch(err => {
-        console.error("Erro ao conectar a collection teacher: ", err);
-        res.status(500);
-    });
+    return teacherModel.getAll(res, query, projection)
 };
 
 exports.getFilteredTeacher = (req,res) => {
@@ -28,89 +15,30 @@ exports.getFilteredTeacher = (req,res) => {
     let projection = {projection: {_id:0, id: 1, name: 1, lastName: 1, phd:1}};
 
     // send to model
-    teacherModel.getFiltered(query, projection)
-    .then(teacher => {
-        if(teacher.length > 0){
-            res.status(200).send(teacher);        
-        }else{
-            res.status(404).send('O professor não foi encontrado');
-        }
-    })
-    .catch(err => {
-        console.error("Erro ao conectar a collection teacher: ", err);
-        res.status(500);
-    });
+    return teacherModel.getFiltered(res, query, projection)
 };
 
 exports.postTeacher = (req, res) => {
-    // check required attributes    
-    if(req.body.name && req.body.lastName){
-
-        // creates teacher array to be inserted        
-        let teacher = {
-            id:0,
-            name:req.body.name,
-            lastName:req.body.lastName,
-            phd:req.body.phd,
-            status:1
-        };
-        
-        // send to model
-        teacherModel.post(teacher)
-        .then(result => {
-            if(result != false){
-                res.status(201).send('Professor cadastrado com sucesso!');
-            }else{
-                res.status(401).send('Não foi possível cadastrar o professor (phd inválido)');
-            }
-        })
-        .catch(err => {
-            console.error("Erro ao conectar a collection teacher: ", err);
-            res.status(500);
-        });
-    }else{
-        res.status(401).send('Não foi possível cadastrar o professor');
-    }
+    // creates teacher array to be inserted        
+    let teacher = {
+        id:0,
+        name:req.body.name,
+        lastName:req.body.lastName,
+        phd:req.body.phd,
+        status:1
+    };
+    
+    // send to model
+    return teacherModel.post(res, teacher) 
 };
 
 exports.putTeacher = (req, res) => {
-    // check required attributes
-    if(req.body.name && req.body.lastName){
-
         //  define query and set for search and update    
         let query = {'id': parseInt(req.params.id), 'status': 1};
         let set = {id:parseInt(req.params.id), name:req.body.name, lastName:req.body.lastName, phd:req.body.phd, status:1};
 
         // send to model
-        teacherModel.put(query, set)
-        .then(async (result) => {
-            if(result.value){ // if professor exists
-                if(result != false){
-                    res.status(200).send('Professor editado com sucesso!');
-
-                     //  updates the course that contains this teacher
-                await courseModel.updateTeacher(parseInt(req.params.id), result.value);
-
-                // receives the updated teacher and updates the student that contains this teacher
-                courseModel.getCoursebyTeacher().then(courses => {
-                    for(var i = 0; i<courses.length; i++){
-                        studentModel.updateTeacher(courses[i]);
-                      }
-            });
-                }else{
-                    res.status(401).send('Não foi possível editar o professor (phd inválido)');
-                }
-            }else{
-                res.status(401).send('Não é possível editar professor inexistente');
-            }
-        })
-        .catch(err => {
-            console.error("Erro ao conectar a collection teacher: ", err);
-            res.status(500);
-        });
-    }else{
-        res.status(401).send('Não foi possível editar o professor');
-    }
+        return teacherModel.put(res, query, set)
 };
 
 exports.deleteTeacher = (req, res) => {
@@ -119,28 +47,5 @@ exports.deleteTeacher = (req, res) => {
     let set = {status:0};
 
     // send to model
-    teacherModel.delete(query, set)
-    .then(async (result) => {
-        //  updates the course that contains that teacher
-        await courseModel.deleteTeacher(parseInt(req.params.id));
-        
-        // receives the updated teacher and updates the student that contains this teacher
-        courseModel.getCoursebyTeacher().then(courses => {
-            for(var i = 0; i<courses.length; i++){
-                studentModel.updateTeacher(courses[i]);
-            }
-        });
-        
-        if(result.value){ // if professor exists
-            console.log('O professor foi removido');
-            res.status(200).send('O professor foi removido com sucesso');
-          }else{
-            console.log('Nenhum professor foi removido');
-            res.status(204).send();
-          }
-    })
-    .catch(err => {
-        console.error("Erro ao conectar a collection teacher: ", err);
-        res.status(500);
-    });
+    return teacherModel.delete(res, query, set)
 };
--- model/teacher.js
+++ model/teacher.js
@@ -1,5 +1,7 @@
 const database = require('../database');
 const collection = database.getCollection('teacher');
+const courseModel = require('../model/course');
+const studentModel = require('../model/student');
 
 var id;
 
@@ -7,37 +9,116 @@ var id;
      id = await collection.countDocuments({});
 })();
 
-exports.getAll = (query, projection) => {
-  return collection.find(query, projection).toArray();
+exports.getAll = (res, query, projection) => {
+  return collection.find(query, projection).toArray()
+  .then(teachers => {
+    if(teachers.length > 0){
+        res.status(200).send(teachers);        
+    }else{
+        res.status(404).send('Nenhum professor cadastrado');
+    }
+  })
+  .catch(err => {
+      console.error("Erro ao conectar a collection teacher: ", err);
+      res.status(500);
+  });
 };
 
-exports.getFiltered = (query, projection) => {
-  return collection.find(query, projection).toArray();
+exports.getFiltered = (res, query, projection) => {
+  return collection.find(query, projection).toArray()
+  .then(teacher => {
+    if(teacher.length > 0){
+        res.status(200).send(teacher);        
+    }else{
+        res.status(404).send('O professor não foi encontrado');
+    }
+  })
+  .catch(err => {
+      console.error("Erro ao conectar a collection teacher: ", err);
+      res.status(500);
+  });
 };
 
-exports.post = (teacher) => {
-  if (teacher.phd == true){
+exports.post = (res, teacher) => {
+  // check required attributes    
+  if (teacher.name && teacher.lastName && teacher.phd == true){
     teacher.id = ++id;
     return collection.insertOne(teacher);  
   }else{
-    return new Promise((resolve, reject) => {
-      resolve(false);
+    res.status(401).send('Não foi possível cadastrar o professor')
+    .then(result => {
+      if(result != false){
+          res.status(201).send('Professor cadastrado com sucesso!');
+      }else{
+          res.status(401).send('Não foi possível cadastrar o professor (phd inválido)');
+      }
+    })
+    .catch(err => {
+        console.error("Erro ao conectar a collection teacher: ", err);
+        res.status(500);
     });
   }
 };
 
-exports.put = (query, set) => {
-  if(set.phd == true){
-    return collection.findOneAndUpdate(query, {$set: set}, {returnOriginal:false});
+exports.put = (res, query, teacher) => {
+  // check required attributes
+  if(teacher.name && teacher.lastName && teacher.phd == true){
+    return collection.findOneAndUpdate(query, {$set: teacher}, {returnOriginal:false})
+    .then(async (result) => {
+      if(result.value){ // if professor exists
+          if(result != false){
+              res.status(200).send('Professor editado com sucesso!');
+
+            //  updates the course that contains this teacher
+            await courseModel.updateTeacher(parseInt(req.params.id), result.value);
+
+          // receives the updated teacher and updates the student that contains this teacher
+          courseModel.getCoursebyTeacher().then(courses => {
+              for(var i = 0; i<courses.length; i++){
+                  studentModel.updateTeacher(courses[i]);
+                }
+      });
+          }else{
+              res.status(401).send('Não foi possível editar o professor (phd inválido)');
+          }
+      }else{
+          res.status(401).send('Não é possível editar professor inexistente');
+      }
+    })
+    .catch(err => {
+        console.error("Erro ao conectar a collection teacher: ", err);
+        res.status(500);
+    });
   }else{
-    return new Promise((resolve, reject) => {
-      resolve(false);
-    });
+    res.status(401).send('Não foi possível editar o professor');
   }
 };
 
-exports.delete = (query, set) => {
-  return collection.findOneAndUpdate(query, {$set: set});
+exports.delete = (res, query, set) => {
+  return collection.findOneAndUpdate(query, {$set: set})
+  .then(async (result) => {
+    //  updates the course that contains that teacher
+    await courseModel.deleteTeacher(parseInt(req.params.id));
+    
+    // receives the updated teacher and updates the student that contains this teacher
+    courseModel.getCoursebyTeacher().then(courses => {
+        for(var i = 0; i<courses.length; i++){
+            studentModel.updateTeacher(courses[i]);
+        }
+    });
+    
+    if(result.value){ // if professor exists
+        console.log('O professor foi removido');
+        res.status(200).send('O professor foi removido com sucesso');
+      }else{
+        console.log('Nenhum professor foi removido');
+        res.status(204).send();
+      }
+})
+.catch(err => {
+    console.error("Erro ao conectar a collection teacher: ", err);
+    res.status(500);
+});
 };
 
 exports.getTeacher = (id) => {
