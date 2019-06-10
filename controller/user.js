0a23af8ec27e7a6c23dc8ebfd62122c42bbc5cc1
jknvlvxs <julio619alves@gmail.com>
2019-06-10 16:57:53 -0300

    adds joi validate schema to required fields in user

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,11 @@
 const userModel = require('../model/user');
+const Joi = require('joi');
+
+const schemaUser = Joi.object().keys({
+	name: Joi.string().required(),
+	lastName: Joi.string().required(),
+	profile: Joi.string().required(),
+});
 
 exports.getAllUsers = (req, res) => {
 	//  define query and projection for search
@@ -19,16 +26,34 @@ exports.getFilteredUser = (req,res) => {
 };
 
 exports.postUser = (req, res) => {
-	// send to model
-	return userModel.post(req, res)
+	Joi.validate(req.body, schemaUser, (err, result) =>{
+		if(!err){
+			// send to model
+			return userModel.post(req, res)
+		}else{
+			res.status(422).json({
+				message: 'Não foi possível inserir o usuário', 
+				error: err.details[0].message
+			});
+		}
+	});
 };
 
 exports.putUser = (req, res) => {
 	//  define query and set for search and update    
 	let query = {'id': parseInt(req.params.id), 'status': 1};
-		
-	// send to model
-	return userModel.put(req, res, query)
+
+	Joi.validate(req.body, schemaUser, (err, result) =>{
+		if(!err){
+			// send to model
+			return userModel.put(req, res, query);
+		}else{
+			res.status(422).json({
+				message: 'Não foi possível editar o usuário', 
+				error: err.details[0].message
+			});
+		}
+	});
 };
 
 exports.deleteUser = (req, res) => {
