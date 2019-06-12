3140bfe65f1b9ce745920586f6d812dc2a49527f
jknvlvxs <julio619alves@gmail.com>
2019-06-12 12:28:16 -0300

    remove versionKey from mongoose

--- model/schema.js
+++ model/schema.js
@@ -7,7 +7,7 @@ userSchema = new Schema({
 	lastName: {type: String},
 	profile: {type: String, enum:{values:['guess','admin'], message: 'O profile {VALUE} é inválido'}},
 	status: {type: Number}
-});
+}, {versionKey: false});
 
 teacherSchema = new Schema({
 	id: {type: Number, unique: true},
@@ -15,7 +15,7 @@ teacherSchema = new Schema({
 	lastName: {type: String},
 	phd: {type: Boolean, validate: [val => {return val == true}, 'É obrigatório o professor possuir phd']},
 	status: {type: Number}
-});
+}, {versionKey: false});
 	
 courseSchema = new Schema({
 	id: {type: Number, unique: true},
@@ -24,7 +24,7 @@ courseSchema = new Schema({
 	city: {type: String},
 	teacher: {type:[teacherSchema], validate: [val => {return val.length >= 2}, 'São necessários pelo menos 2 professores válidos']},
 	status: {type: Number}
-});
+}, {versionKey: false});
 	
 studentSchema = new Schema({
 	id: {type: Number, unique: true},
@@ -33,6 +33,6 @@ studentSchema = new Schema({
 	age: {type: Number, min:[17, 'A idade inserida é {VALUE} anos de idade, a idade mínima permitida é 17!']},
 	course: {type:[courseSchema], validate: [val => {return val.length == 1}, 'É necessário inserir um curso válido']},
 	status: {type: Number}
-});
+}, {versionKey: false});
 
 module.exports = {userSchema, teacherSchema, courseSchema, studentSchema};
