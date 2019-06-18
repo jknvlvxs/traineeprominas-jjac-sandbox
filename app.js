d5bba79b241079c18f3f7c9fdde25a7abee3fd4b
jknvlvxs <julio619alves@gmail.com>
2019-06-18 17:38:51 -0300

    allowing CORS

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const database = require('./database');
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+   });
+
 database //connection to database.js
 .connect()
 // .then(() => {
